3ecde914b74e13cbfd1fe48fcaadfba900d8c692
treilhes <p.treilhes@free.fr>
2024-05-29 14:33:51 +0200

    fix KeyboardModifier missing/moved

--- jfxapps.ext/jfxapps.ext.container/src/main/java/com/gluonhq/jfxapps/ext/container/menu/SceneBuilderMenuProvider.java
+++ jfxapps.ext/jfxapps.ext.container/src/main/java/com/gluonhq/jfxapps/ext/container/menu/SceneBuilderMenuProvider.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2016, 2023, Gluon and/or its affiliates.
- * Copyright (c) 2021, 2023, Pascal Treilhes and/or its affiliates.
+ * Copyright (c) 2016, 2024, Gluon and/or its affiliates.
+ * Copyright (c) 2021, 2024, Pascal Treilhes and/or its affiliates.
  * Copyright (c) 2012, 2014, Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
@@ -40,7 +40,7 @@ import org.springframework.context.annotation.Lazy;
 
 import com.gluonhq.jfxapps.boot.context.annotation.ApplicationInstanceSingleton;
 import com.gluonhq.jfxapps.boot.platform.JfxAppsPlatform;
-import com.gluonhq.jfxapps.core.action.editor.KeyboardModifier;
+import com.gluonhq.jfxapps.core.api.action.editor.KeyboardModifier;
 import com.gluonhq.jfxapps.core.api.i18n.I18N;
 import com.gluonhq.jfxapps.core.api.ui.menu.MenuItemAttachment;
 import com.gluonhq.jfxapps.core.api.ui.menu.MenuItemProvider;
