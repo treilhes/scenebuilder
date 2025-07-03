96d75f76e0dfe0efead9dee95068abf43bf84635
treil <treil@B550M_RZ5600G.mshome.net>
2025-07-04 00:15:18 +0200

    update github workflows from source master

--- .github/scripts/jpackage.bat
+++ .github/scripts/jpackage.bat
@@ -1,4 +1,4 @@
-for /F %%i in ('%JAVA_HOME%\bin\jdeps --module-path %JAVAFX_HOME% --print-module-deps --ignore-missing-deps %GITHUB_WORKSPACE%/app/target/lib/scenebuilder-%VERSION%-all.jar') do SET JDEPS_MODULES=%%i
+for /F %%i in ('%JAVA_HOME%\bin\jdeps --module-path %JAVAFX_HOME% --print-module-deps --ignore-missing-deps %GITHUB_WORKSPACE%/app/target/lib/scenebuilder-%APP_VERSION%-all.jar') do SET JDEPS_MODULES=%%i
 
 REM jdeps doesn't include JavaFX modules in Windows
 set JAVAFX_MODULES=javafx.fxml,javafx.media,javafx.swing,javafx.web
@@ -8,13 +8,13 @@ REM set MODULES=java.desktop,java.logging,java.naming,java.prefs,java.security.j
 --module-path %JAVAFX_HOME% ^
 --add-modules %JDEPS_MODULES%,%JAVAFX_MODULES% ^
 --output app/target/runtime ^
---strip-debug --compress 2 --no-header-files --no-man-pages
+--strip-debug --compress zip-6 --no-header-files --no-man-pages
 
 %JPACKAGE_HOME%\bin\jpackage ^
---app-version %VERSION% ^
+--app-version %APP_VERSION% ^
 --input app/target/lib ^
 --license-file LICENSE.txt ^
---main-jar scenebuilder-%VERSION%-all.jar ^
+--main-jar scenebuilder-%APP_VERSION%-all.jar ^
 --main-class %MAIN_CLASS% ^
 --name SceneBuilder ^
 --description "Scene Builder" ^
@@ -23,7 +23,10 @@ REM set MODULES=java.desktop,java.logging,java.naming,java.prefs,java.security.j
 --runtime-image app/target/runtime ^
 --dest %INSTALL_DIR% ^
 --type msi ^
+--java-options "-Djavafx.allowjs=true" ^
 --java-options "--add-opens=javafx.fxml/javafx.fxml=ALL-UNNAMED" ^
+--java-options "--enable-native-access=javafx.graphics" ^
+--java-options "--sun-misc-unsafe-memory-access=allow" ^
 --java-options "-Djava.library.path=runtime\bin;runtime\lib" ^
 --icon app/assets/windows/icon-windows.ico ^
 --win-dir-chooser ^
--- .github/scripts/jpackage.sh
+++ .github/scripts/jpackage.sh
@@ -1,17 +1,17 @@
-jdeps_modules=$(jdeps --module-path $JAVAFX_HOME --print-module-deps --ignore-missing-deps app/target/lib/scenebuilder-$VERSION-all.jar)
+jdeps_modules=$(jdeps --module-path $JAVAFX_HOME --print-module-deps --ignore-missing-deps app/target/lib/scenebuilder-$APP_VERSION-all.jar)
 JAVAFX_MODULES=javafx.fxml,javafx.media,javafx.swing,javafx.web
 
 $JAVA_HOME/bin/jlink \
 --module-path $JAVAFX_HOME \
 --add-modules $jdeps_modules,$JAVAFX_MODULES \
 --output app/target/runtime \
---strip-debug --compress 2 --no-header-files --no-man-pages
+--strip-debug --compress zip-6 --no-header-files --no-man-pages
 
 $JPACKAGE_HOME/bin/jpackage \
---app-version $VERSION \
+--app-version $APP_VERSION \
 --input app/target/lib \
 --license-file LICENSE.txt \
---main-jar scenebuilder-$VERSION-all.jar \
+--main-jar scenebuilder-$APP_VERSION-all.jar \
 --main-class $MAIN_CLASS \
 --name SceneBuilder \
 --description "Scene Builder" \
